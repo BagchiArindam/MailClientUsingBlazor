6410c1faa18a81f57f8af8a77c2a315dac5501f4
Elrondan <Elrondan@TABLET-8CVS0POD>
2024-01-29 08:47:01 +0530

    Add project files.

---
+++ LIbraries/Constants/Constants1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailFetch1.LIbraries.Constants
+{
+    public static class Constants1
+    {
+        public static readonly string email = "";
+        public static readonly string password = "";
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MailFetch1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
